15a05d0d9ca12a9a52a68346d3e42358da360c7f
Anastasia Karmazina <akarmazina082@gmail.com>
2022-06-20 15:54:53 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import Vue from 'vue'
+import App from './App.vue'
+// import store from './vuex/store'
+// import store from './vuex/store'
+// import './assets/styles/styles.scss'
+
+createApp(App).mount('#app')
+Vue.config.Vue.config.productionTip = false
+
+
+// new Vue({
+//     render: h => h(App),
+//     store,
+//   }).$mount( '#app')
+  
